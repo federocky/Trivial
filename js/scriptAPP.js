c2a8152b8e5cd398484ca8b212cce613a600ba53
Federico Jácome <fede_rock_85@hotmail.com>
2020-10-23 21:12:24 +0200

    intentando hacer responsive pero faltan ajustes

--- js/scriptAPP.js
+++ js/scriptAPP.js
@@ -37,23 +37,24 @@ window.addEventListener('load', function(){
 
 
 
-
-    var contador = document.querySelector('.contador');
-    var valorContador = 2;
+    const contenedorBoton = document.getElementById('contenedorBoton');
+    const contador = document.querySelector('.contador');
+    var valorContadorInicial = 5;
 
     const contenedor = document.querySelector('#contenedor');
 
     const barraTiempo = document.querySelector('#barraProgreso > div');
+    var tiempo = 10;
     
     const pregunta = document.getElementById('pregunta');
     const respuesta1 = document.getElementById('respuesta1');
     const respuesta2 = document.getElementById('respuesta2');
     const respuesta3 = document.getElementById('respuesta3');
     const respuesta4 = document.getElementById('respuesta4');
-    var preguntasRespondidas = [];
+    const respuestas = document.querySelectorAll('.respuesta');
+    
     var respuestaBuena = ' ';
-    contador.addEventListener('click', countdown);
-
+    
     const marcadorPlayer1 = document.getElementById('puntosP1');
     const marcadorPlayer2 = document.getElementById('puntosP2');
     var puntosPlayer1 = 0;
@@ -62,58 +63,82 @@ window.addEventListener('load', function(){
     var turnos = 0;
     var finPartida = 4;
     var cambioTurno = finPartida/2;
-
+    
+    contador.addEventListener('click', countdown);
     respuesta1.addEventListener('click', comprobarRespuesta);
     respuesta2.addEventListener('click', comprobarRespuesta);
     respuesta3.addEventListener('click', comprobarRespuesta);
     respuesta4.addEventListener('click', comprobarRespuesta);
 
     /**
-     * Cuenta atras al entrar.
+     * Cuenta atras al entrar que inicia la partida
      */
     function countdown(){
         contador.classList.remove('contadorSinPulsar');
-        contador.classList.add('contadorPulsado');
-        contador.textContent = valorContador;
+        //contador.classList.add('contadorPulsado');
+        contador.textContent = valorContadorInicial;
 
 
     let intervalo = setInterval(function(){
-            valorContador--;
-            contador.textContent = valorContador
-            if(valorContador == 0) {
+            valorContadorInicial--;
+            contador.textContent = valorContadorInicial;
+            if(valorContadorInicial == 0) {
                 clearInterval(intervalo);
-                contador.style.display = 'none';
-                contenedor.style.visibility = 'visible';
-                cambiaPregunta();
+                contador.textContent = 'Turno del player 1'
+                setTimeout(() => {
+                    contenedorBoton.style.display = 'none';
+                    contenedor.style.visibility = 'visible';
+                    cambiaPregunta();
+                }, 2000);
             }
         },1500);
+        contador.removeEventListener('click', countdown);
     }
 
-    function cambiaPregunta(){
-        barraTiempo.style.display = 'flex';
 
-        let posicion = Math.floor(Math.random() * (preguntas.length));
+    /**
+     * Función que carga una pregunta al azar de el array y la elimina de este para que no se repita. 
+     */
+    function cambiaPregunta(){
+        barraTiempo.style.display = 'flex'; //Muestra la barra de tiempo
+        tiempo = 10;
+
+        let posicion = Math.floor(Math.random() * (preguntas.length)); //Genero el numero random que sera la posicion de la pregunta
 
             pregunta.innerHTML = preguntas[posicion].pregunta;
             respuesta1.innerHTML = preguntas[posicion].respuesta1;
             respuesta2.innerHTML = preguntas[posicion].respuesta2;
             respuesta3.innerHTML = preguntas[posicion].respuesta3;
             respuesta4.innerHTML = preguntas[posicion].respuesta4;
-            respuestaBuena = preguntas[posicion].respuestaCorrecta;
-            preguntasRespondidas.push(posicion);
+            respuestaBuena = preguntas[posicion].respuestaCorrecta; //esta es la respuesta correcta
+            //preguntasRespondidas.push(posicion);
 
-            preguntas.splice(posicion,1);
+            preguntas.splice(posicion,1); //eliminamos la pregunta del array
+            
+
+            var tiempoRespuesta = setInterval(() => {
+                tiempo--;
+                if (tiempo == 0) {
+                    comprobarQueHacer();
+                    clearInterval(tiempoRespuesta);
+                }
+            }, 1000);
 
     }
 
 
-
+    /**
+     * Una vez seleccionada la respuesta debemos comprobar si es correcta o no comparandola con nuestra variable
+     * de respuesta buena. 
+     * @param {} e 
+     */
     function comprobarRespuesta(e){
-        barraTiempo.style.display = 'none';
+        //barraTiempo.style.display = 'none'; //quitamos la barra de tiempo
 
         if (e.target.id == respuestaBuena) {
             e.target.classList.add('respuestaCorrecta');
-
+            
+            //a continuacion si hemos acertado no suma puntos. 
             if(turnos < cambioTurno) {
                 puntosPlayer1++;
                 marcadorPlayer1.innerHTML = puntosPlayer1;
@@ -122,42 +147,74 @@ window.addEventListener('load', function(){
                 marcadorPlayer2.innerHTML = puntosPlayer2;
             }
 
-        }else e.target.classList.add('respuestaIncorrecta');
+        }else {
+            e.target.classList.add('respuestaIncorrecta');
+            for (const elemento of respuestas) {
+                if (elemento.id == respuestaBuena) elemento.classList.add('respuestaCorrecta');
+            }
+        }
+    
+        comprobarQueHacer();
+    }
 
-        
+    function comprobarQueHacer() {
         turnos++;
-        if(turnos == cambioTurno) document.getElementById('turno').innerHTML = 'Player 2';
-
+        barraTiempo.style.display = 'none'; //quitamos la barra de tiempo
         
+        for (const elemento of respuestas) {
+            if (elemento.id == respuestaBuena) elemento.classList.add('respuestaCorrecta');
+        }
+
         if (turnos == finPartida) terminarPartida();
-        else {
+        else if (turnos == cambioTurno){
+            cambioDeTurno();
+        } else {
             setTimeout(() => {
-                e.target.className = 'respuesta';
+                quitarClase(); //quitamos la clase de correcta o incorrecta
                 cambiaPregunta();
             }, 2000);
         }
-        
+    }
+
+    function cambioDeTurno(){
+        setTimeout(() => {
+            //e.target.className = 'respuesta'; //quitamos la clase de respuesta correcta o incorrecta
+            quitarClase(); //quitamos la clase de correcta o incorrecta
+            document.getElementById('turno').innerHTML = 'Player 2';
+            contenedor.style.visibility = 'hidden';
+            contador.textContent = 'Turno del player 2' //avisamos que comienza el turno del player 2. 
+            contenedorBoton.style.display = 'flex';
+            setTimeout(() => {
+                contenedor.style.visibility = 'visible';
+                contenedorBoton.style.display = 'none';
+                cambiaPregunta();
+            }, 3300);
+        }, 1500);
+
     }
     
+
+    /**
+     * Cuando se acaban los turnos comprobamos quien tiene mas respuestas correctas y mostramos por pantalla. 
+     */
     function terminarPartida() {
         let ganador;
             if (puntosPlayer1 > puntosPlayer2) ganador = 'Ganador player 1';
             else if (puntosPlayer1 < puntosPlayer2) ganador = 'Ganador player 2';
             else ganador = 'empate';
 
-            contenedor.style.visibility = 'hidden';
-            let span = document.createElement('span');
-            span.innerHTML = ganador;
-            document.querySelector('body').appendChild(span);
+            setTimeout(() => {
+                contenedor.style.visibility = 'hidden';
+                contador.textContent = ganador;
+                contenedorBoton.style.display = 'flex';
+            }, 1000);
     }
 
 
-
-    function contiene (array, valor) {
-        for (const elemento of array) {
-            if (elemento == valor) return true;
+    function quitarClase(){
+        for (const elemento of respuestas) {
+            elemento.className = 'respuesta';
         }
-        return false;
     }
 
 
